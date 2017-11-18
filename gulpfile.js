4f40db53581669a22a0d8131051cd44e4d8819bc
Mauricio Jovel <mjovel@applaudostudios.com>
2017-11-17 19:17:45 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+// http://david-barreto.com/working-with-sass-bootstrap-and-gulp/
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var inject = require('gulp-inject');
+var wiredep = require('wiredep').stream;
+ 
+gulp.task('styles', function(){
+    var injectAppFiles = gulp.src('src/styles/*.scss', {read: false});
+    
+     function transformFilepath(filepath) {
+       return '@import "' + filepath + '";';
+     }
+    
+     var injectAppOptions = {
+       transform: transformFilepath,
+       starttag: '// inject:app',
+       endtag: '// endinject',
+       addRootSlash: false
+     };
+    
+     return gulp.src('src/main.scss')
+        .pipe(wiredep())
+        .pipe(inject(injectAppFiles, injectAppOptions))
+        .pipe(sass())
+        .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('html', ['styles'], function(){
+    var injectFiles = gulp.src(['dist/styles/main.css']);
+
+    var injectOptions = {
+        addRootSlash: false,
+        ignorePath: ['src', 'dist']
+    };
+
+    return gulp.src('src/index.html')
+        .pipe(inject(injectFiles, injectOptions))
+        .pipe(gulp.dest('dist'));
+});
